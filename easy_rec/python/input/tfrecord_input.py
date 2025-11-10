31c6ea488c00b33e4e7891daeb0ef2559e58123b
chensheng <15910216103@163.com>
2025-11-10 18:22:06 +0800

    完成离线推理本地测试

--- easy_rec/python/input/tfrecord_input.py
+++ easy_rec/python/input/tfrecord_input.py
@@ -3,6 +3,7 @@
 import logging
 
 import tensorflow as tf
+from tensorflow.python.ops import array_ops
 
 from easy_rec.python.input.input import Input
 from easy_rec.python.utils.tf_utils import get_tf_type
@@ -26,6 +27,8 @@ class TFRecordInput(Input):
                          task_num, check_mode, pipeline_config)
 
     self.feature_desc = {}
+
+    self.input_dim_dic = {}
     for x, t, d, s in zip(self._input_fields, self._input_field_types,
                           self._input_field_defaults, self._input_dims):
       d = self.get_type_defaults(t, d)
@@ -36,6 +39,7 @@ class TFRecordInput(Input):
       else:
         self.feature_desc[x] = tf.FixedLenFeature(
             dtype=t, shape=[s], default_value=[d] * s)
+      self.input_dim_dic[x] = s
 
   def _parse_tfrecord(self, example):
     try:
@@ -98,3 +102,61 @@ class TFRecordInput(Input):
     else:
       dataset = dataset.map(lambda x: (self._get_features(x)))
     return dataset
+
+  def create_multi_placeholders(self, export_config):
+    """Create multiply placeholders on export, one for each feature.
+
+    Args:
+      export_config: ExportConfig instance.
+    """
+    self._mode = tf.estimator.ModeKeys.PREDICT
+
+    if export_config.auto_multi_value:
+      export_fields_name = self._multi_value_fields
+    elif export_config.multi_value_fields:
+      export_fields_name = export_config.multi_value_fields.input_name
+    else:
+      export_fields_name = None
+    placeholder_named_by_input = export_config.placeholder_named_by_input
+
+    sample_weight_field = ''
+    if self._data_config.HasField('sample_weight'):
+      sample_weight_field = self._data_config.sample_weight
+
+    if export_config.filter_inputs:
+      effective_fids = list(self._effective_fids)
+    else:
+      effective_fids = [
+          fid for fid in range(len(self._input_fields))
+          if self._input_fields[fid] not in self._label_fields and
+          self._input_fields[fid] != sample_weight_field
+      ]
+
+    inputs = {}
+    for fid in effective_fids:
+      input_name = self._input_fields[fid]
+      if input_name == sample_weight_field:
+        continue
+      if placeholder_named_by_input:
+        placeholder_name = input_name
+      else:
+        placeholder_name = 'input_%d' % fid
+      if input_name in export_fields_name:
+        tf_type = self._multi_value_types[input_name] if input_name in self._multi_value_types \
+            else get_tf_type(self._input_field_types[fid])
+        logging.info('multi value input_name: %s, dtype: %s' %
+                     (input_name, tf_type))
+        finput = array_ops.placeholder(
+            tf_type, [None, None], name=placeholder_name)
+      else:
+        ftype = self._input_field_types[fid]
+        tf_type = get_tf_type(ftype)
+        logging.info('input_name: %s, dtype: %s' % (input_name, tf_type))
+        # finput = array_ops.placeholder(tf_type, [None], name=placeholder_name)
+        finput = array_ops.placeholder(
+            tf_type, (None, self.input_dim_dic[input_name]),
+            name=placeholder_name)
+      inputs[input_name] = finput
+    features = {x: inputs[x] for x in inputs}
+    features = self._preprocess(features)
+    return inputs, features['feature']
